949b412b838495b32df0ceeb82216bbd32464e4a
Hien110 <minhhien30201@gmail.com>
2026-03-26 14:20:05 +0700

    fix(component): update variant to 'stacked' in ProductPage and adjust imageGallery styles

--- src/pages/products/ProductPage.tsx
+++ src/pages/products/ProductPage.tsx
@@ -101,7 +101,7 @@ const ProductPage = () => {
     {
       type: "product",
       tab: t("sections.napEOE.tab"),
-      variant: "feature",
+      variant: "stacked",
       props: {
         title: t("sections.napEOE.title"),
         description: t("sections.napEOE.desc"),
